1ea162ba803b04708c2ad135ddae51ec8382a3a4
elisa-esteban <elisa.esteban.segurado@gmail.com>
2017-02-15 11:27:39 +0100

    Change examples

--- R/StatRegDiffTSPred.R
+++ R/StatRegDiffTSPred.R
@@ -43,9 +43,9 @@
 #' StatRegDiffTSPred(Mat, forward = 1L)
 #'
 #' # With an object of class StQList
-#' data(DepuracionExample)
+#' data(StQList_Example)
 #' VarNames <- c('ActivEcono_35._6._2.1.4._0', 'GeoLoc_35._6._2.1._1.2.5.')
-#' StatRegDiffTSPred(DepuracionExample, VarNames = VarNames)
+#' StatRegDiffTSPred(StQList_Example, VarNames = VarNames)
 #'
 #' @export
 setGeneric("StatRegDiffTSPred", function(x,  StatDiff = 12L, forward = 2L,
@@ -146,6 +146,7 @@ setMethod(
 
       VarNames <- ExtractNames(VarNames)
       Data.list <- getData(x, VarNames)
+      IDQuals <- unlist(lapply(Data.list, getIDQual))
       DD <- getDD(Data.list[[length(Data.list)]])
       Data.list <- lapply(Data.list, getData)
 
@@ -159,9 +160,10 @@ setMethod(
       for (i in 1:length(VarNames)){
 
           key <- DD[Variable == VarNames[i]]
-          Quals <- setdiff(names(key), c('Variable', 'Sort', 'Class', 'Length', 'Qual1', 'ValueRegExp'))
+          Quals <- setdiff(names(key), c('Variable', 'Sort', 'Class', 'Length', 'ValueRegExp'))
           key <- transpose(key[, Quals, with = FALSE])[['V1']]
-          keyVar[[i]] <- key[key != '']
+          key <- key[key != '']
+          keyVar[[i]] <- setdiff(key, IDQuals)
 
       }
 
